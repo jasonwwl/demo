a65044b2aa84f5ab822def53550577f40236141c
JasonWang <jasonwx@163.com>
2016-12-18 13:43:12 +0800

    change demo server port to 8001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-server.listen(8088);
+server.listen(8001);
 
 app.get('/', (req, res) => {
   res.send('welcome');
